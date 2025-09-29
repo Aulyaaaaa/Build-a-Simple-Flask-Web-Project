02ea47ac8a8c2b262a4a91d93a47478802536a70
Aulyaaaaa <aulll3446@gmail.com>
2025-09-29 23:34:54 +0700

    save progres: fixing blueprint references

--- board/__init__.py
+++ board/__init__.py
@@ -1,12 +1,17 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
+
+from board import pages, posts
+
+load_dotenv()
 
 def create_app():
   app = Flask(__name__)
-  
-  from board.posts import posts_bp
-  from board.pages import pages_bp
+  app.config.from_prefixed_env()
 
-  app.register_blueprint(posts_bp)
-  app.register_blueprint(pages_bp)
- 
+  app.register_blueprint(pages.bp)
+  app.register_blueprint(posts.bp)
+  print(f"Current Environment: {os.getenv('ENVIRONMENT')}")
+  print(f"Using Database: {app.config.get('DATABASE')}")
   return app 
--- board/pages.py
+++ board/pages.py
@@ -1,15 +1,15 @@
 from flask import Blueprint, render_template, redirect, url_for
 
-pages_bp = Blueprint("pages", __name__)
+bp = Blueprint("pages", __name__)
 
-@pages_bp.route("/")
+@bp.route("/")
 def home():
  return redirect(url_for("posts.create_post"))
 
-@pages_bp.route("/about")
+@bp.route("/about")
 def about():
  return render_template("pages/about.html")
 
-@pages_bp.route("/contact")
+@bp.route("/contact")
 def contact():
     return render_template("pages/contact.html")
